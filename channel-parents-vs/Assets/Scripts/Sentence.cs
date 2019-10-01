3b7c5705b4308f408da4a55d6173423c80f1ed80
Malek <maleka@qatar.cmu.edu>
2019-10-01 05:02:06 +0300

    Barebones dialogue

---
+++ channel-parents-vs/Assets/Scripts/Sentence.cs
@@ -0,0 +1,84 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Line
+{
+
+    /* ----- CONTENTS AND APPEARANCE -----*/
+
+    private string text; //Text to be printed
+
+    //Font
+    //Color
+
+    /* ----- DRAWING SPEED AND EFFECTS -----*/
+
+    //How many seconds to wait before printing the next character.
+    private float drawing_speed; 
+
+    //Shaking effects?
+    //Text wrapping? This currently seems unnecessary
+    //Handling multiple pages of text?
+
+    public Line(string text_, float drawing_speed_)
+    {
+        text = text_;
+        drawing_speed = drawing_speed_;
+    }
+
+    public string GetText()
+    {
+        return text;
+    }
+
+    public float GetDrawingSpeed()
+    {
+        return drawing_speed;
+    }
+
+}
+
+//A Sentence is a list of Lines; each Line has it's own text, drawing speed,
+//font, color, effects, etc. 
+public class Sentence
+{
+    private List<Line> sentence;
+    //Two kinds of triggers as to when to print a Sentence:
+    //Choice trigger: Went through door 1, just entered a room, etc;
+    //Reactive triggers: Head turned left in Room 29, walked away from parent, etc;
+    //Of course, choice triggers are easy to handle, but what of reactive triggers?
+
+    private int current_index = 0;
+
+    public Sentence(Line[] lines)
+    {
+        sentence = new List<Line>();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            sentence.Add(lines[i]);
+        }
+
+    }
+
+    //For simple sentences
+    public Sentence(string whatever, float whatever2)
+    {
+        sentence = new List<Line>();
+        Line myLine = new Line(whatever, whatever2);
+        sentence.Add(myLine);
+    }
+
+    public Line GetNext()
+    {
+        //current_index += 1;
+        return sentence[current_index];
+    }
+
+    public bool IsFinished()
+    {
+        return !(current_index < sentence.Count);
+    }
+}
---
+++ channel-parents-vs/Assets/Scripts/SentenceManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SentenceManager : MonoBehaviour
+{
+
+
+    private List<Sentence> sentences; //How to load up elegantly?
+    private Sentence first;
+    private Sentence test1;
+    private Sentence test2;
+    private int current_sentence = 1;
+
+
+    private Text text_comp;
+    private bool updating_text = false; //True if "printing", or updating text box
+    private bool skip_effects = false; //If true, instantly fill the dialogue box.
+    void Start()
+    {
+        text_comp = this.GetComponent<Text>();
+        first = new Sentence("Press enter for the next line...", 0.0f);
+        this.GetComponent<Text>().text = first.GetNext().GetText();
+
+        test1 = new Sentence("This is a very long sentence to demonstrate the existence of text wrapping in the current system", 0.01f);
+        test2 = new Sentence("Is this?", 0.05f);
+        sentences = new List<Sentence>();
+        //Clever load up should be here
+        sentences.Add(test1);
+        sentences.Add(test2);
+    }
+
+    //Checks to see if a Sentence needs to be printed onto the text box
+    //Currently just auto prints a sentence
+    void CheckTrigger()
+    {
+        int num_sentences = sentences.Count;
+        int cur_index = 0;
+
+        while (cur_index < num_sentences)
+        {
+            StartCoroutine(DrawSentence(sentences[current_sentence]));
+            return;
+        }
+
+    }
+
+    IEnumerator DrawSentence(Sentence sentence)
+    {
+        Line current_line;
+        string text;
+        float drawing_speed;
+
+        current_line = sentence.GetNext();
+        text = current_line.GetText();
+        drawing_speed = current_line.GetDrawingSpeed();
+
+        //Used when slow drawing is enabled; keeps track of how much of the string to print.
+        int current_index = 0;
+
+        while (true)
+        {
+
+            if (drawing_speed == 0.0f || skip_effects)
+            {
+                text_comp.text = text;
+                current_index = text.Length;
+                skip_effects = false;
+            }
+
+            if (current_index < text.Length)
+            {
+                text_comp.text = text.Substring(0, current_index + 1);
+                current_index++;
+                yield return new WaitForSeconds(drawing_speed);
+            } else
+            {
+                updating_text = false;
+                yield break;
+            }
+
+        } 
+    }
+
+
+    void Update() //For drawing speed and other fancy effects, this is a must.
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (updating_text)
+            {
+                skip_effects = true;
+            } else
+            {
+                current_sentence = (current_sentence == 0) ? 1 : 0; //Garbage currently
+                CheckTrigger();
+                updating_text = true;
+            }
+        }
+    }
+}
